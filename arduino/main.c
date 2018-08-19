c827953b2d0ab05eb6ff0d67572db16377212516
Ickson <ickson94@gmail.com>
2018-08-19 20:01:36 -0300

    Compilado...

--- arduino/main.c
+++ arduino/main.c
@@ -1,29 +1,35 @@
-#ifndef F_CPU
-#define F_CPU 16000000UL // Frequencia de 16MHz
-#endif
-
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 #include <stdio.h>
 #include "serial.h"
+#include "timers.h"
+#include "adc.h"
 
 int main(void)
 {
-	Serial_Init(9600);
+	//Serial_Init(9600);
 
 	DDRB |= (1<<PB5);
+	delay_ms(1000);
+	//Serial_writeFlush(0x01);
 
-	_delay_ms(1000);
 	while(1)
 	{
-		if(Serial_read() == 0x01)
+		/*if(Serial_read() == 0x01)
 		{
 			PORTB ^= (1<<PB5);
-			_delay_ms(1000);
+			delay_ms(1000);
 			Serial_writeFlush(0x01);
-			_delay_ms(10);
-		}
+			delay_ms(1000);
+		}*/
+		/*if(ADC_Porcentagem(0) > (char) 50)
+		//if(ADC_Read(0) > 256)
+			PORTB |= (1<<PB5);
+		else
+			PORTB &= ~(1<<PB5);
+		delay_ms(100);*/
+		PORTB ^= (1<<PB5);
+		delay_ms(1000);
 	}
 }
