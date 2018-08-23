bd987307e2510e5a344f910dd3bb814758943aa5
Ickson <ickson94@gmail.com>
2018-08-23 10:23:51 -0300

    Testes...

--- arduino/main.c
+++ arduino/main.c
@@ -1,35 +1,97 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/boot.h>
 #include <stdio.h>
-#include "serial.h"
-#include "timers.h"
-#include "adc.h"
+#include <string.h>
+#include "lib/serial.h"
+#include "lib/timers.h"
+#include "lib/adc.h"
+#include "lib/spi.h"
+#include "sdcard/diskio.h"
+
+
+char ENDERECO_ESCRAVO = 0;
+
 
 int main(void)
 {
-	//Serial_Init(9600);
+
+	uint8_t ram_buffer[SPM_PAGESIZE];
+	unsigned char c;
+
+	Serial_Init(9600);
 
 	DDRB |= (1<<PB5);
-	delay_ms(1000);
+	delay_us(2000);
 	//Serial_writeFlush(0x01);
+	Serial_string("Iniciou...\r\n");
+
+	int i = 0;
+	char buffer[50];
+	BYTE values[128];
+	BYTE values2[128];
+
+	if(!disk_initialize())
+	{
+		//Serial_string("Dados da flash: \r\n");
+		disk_readp(values, (DWORD) 1, 0, 128);
+		for(i=0; i<128; i++)
+		{
+			sprintf(buffer, "%d\r\n", values[i]);
+			Serial_string(buffer);
+		}
+
+		for(i=0; i<128; i++)
+			values2[i] = i;
+
+		/*sprintf(buffer, "Escrita: %d\r\n", ));
+		Serial_string(buffer);
+
+		sprintf(buffer, "Escrita: %d\r\n", disk_readp(values, (DWORD) 1, 0, 128));
+		Serial_string(buffer);
+		*/
+		//Serial_string("Dados da flash: \r\n");
+		disk_writep(0, (DWORD) 1);
+		disk_writep(values2, (DWORD) 128);
+		disk_writep(0,0);
+		
+		disk_readp(values, (DWORD) 1, 0, 128);
+		for(i=0; i<128; i++)
+		{
+			sprintf(buffer, "%d\r\n", values[i]);
+			Serial_string(buffer);	
+		}
+
+	}
+	else
+	{
+		Serial_string("Erro ao iniciar disco...\r\n");
+	}
+	delay_ms(100); // wait for sending all text via serial
+	
+
+	
+	
+
+	/*for (i=0;i<SPM_PAGESIZE;i++) 
+	{
+		if (i % 2 == 0) 
+		{ // we must write WORDs
+	    	w = i;
+		} 
+		else 
+		{
+	    	w += (ram_buffer[i] << 8);
+			optiboot_page_fill((optiboot_addr_t)(void*) i+144,w);
+		}
+	}
+	*/
+	Serial_string("Gravado...");
 
 	while(1)
 	{
-		/*if(Serial_read() == 0x01)
-		{
-			PORTB ^= (1<<PB5);
-			delay_ms(1000);
-			Serial_writeFlush(0x01);
-			delay_ms(1000);
-		}*/
-		/*if(ADC_Porcentagem(0) > (char) 50)
-		//if(ADC_Read(0) > 256)
-			PORTB |= (1<<PB5);
-		else
-			PORTB &= ~(1<<PB5);
-		delay_ms(100);*/
-		PORTB ^= (1<<PB5);
-		delay_ms(1000);
+		delay_us(2000);
 	}
 }
